62dd43659f2dd85309467b4ddd7350edf351c127
NazarTymoshchuk <nazar.tymo@gmail.com>
2025-09-08 19:33:30 +0300

    style Order page

--- my-app/src/pages/OrderPage.js
+++ my-app/src/pages/OrderPage.js
@@ -22,34 +22,58 @@ function OrderPage() {
             <h2>Order Page</h2>
 
             {user.orders.length === 0 ? "No orders yet" : 
-                user.orders.map((o) => (
-                    <div>
-                        <div className="order-info">
-                            <div>{o.id}</div>
-                            <div>{o.date}</div>
-                            <div>{o.address}</div>
-                            <div>{o.totalAmount}</div>
-                            <Button
-                                onClick={() => toggleOrder(o.id)}
-                                aria-controls={`collapse-${o.id}`}
-                                aria-expanded={!!openOrders[o.id]}
-                            >
-                                click
-                            </Button>
-                        </div>
-                        <Collapse in={!!openOrders[o.id]}>
-                            <div id={`collapse-${o.id}`}>
-                                {o.orderProducts.map((p) => (
-                                    <div>
-                                        <div>{p.name}</div>
-                                    </div>
-                                ))}
-                            </div>
-                        </Collapse>
+                <div>
+                    <div className="order-header">
+                        <div>Date</div>
+                        <div>Address</div>
+                        <div>Total Amount</div>
+                        <div></div>
                     </div>
-                    
-                ))
-            }
+                    <hr className="order-hr"/>
+                    {user.orders.map((o) => (
+                        <div>
+                            <div className="order-container">
+                                <div className="order-id">№ {o.id}</div>
+                                <hr />
+                                <div className="order-info">
+                                    <div>{o.date}</div>
+                                    <div>{o.address}</div>
+                                    <div>{o.totalAmount}</div>
+                                    <Button
+                                        onClick={() => toggleOrder(o.id)}
+                                        variant="outline-light"
+                                        className="btn-order"
+                                        aria-expanded={!!openOrders[o.id]}
+                                    >
+                                        Details <i class="bi bi-chevron-down"></i>
+                                    </Button>
+                                </div>
+                                <Collapse in={!!openOrders[o.id]}>
+                                    <div>
+                                        <hr />
+                                        <div className="order-products-header">
+                                            <div></div>
+                                            <div>Title</div>
+                                            <div>Quantity</div>
+                                            <div>Amount</div>
+                                        </div>
+                                        {o.orderProducts.map((p) => (
+                                            <div className="order-products">
+                                                <img src={p.images[0]} alt="" />
+                                                <div>{p.name}</div>
+                                                <div>{p.quantity}</div>
+                                                <div>{p.price}</div>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </Collapse>
+                            </div>
+                            
+                        </div>
+                    ))}
+                </div>
+                
+                }
 
             
         </div>
